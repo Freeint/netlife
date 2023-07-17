588053e4625d2c76c3c47bc078eafcbb5e4efe36
dunnyc <111081087+dunnyc@users.noreply.github.com>
2023-07-17 15:39:08 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var password = 'Hb2xt2788';
+function passcheck(){
+    if(document.getElementById('pass1').value != password) {
+        alert('wrong password');
+        return false;
+    }
+
+
+if(document.getElementById('pass1').value != password) {
+    alert('Correct password. Click ok to neter webpage.');
+}
+}
